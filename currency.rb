52109b00d063f9b38abb0cded6f5930bef035870
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-04 15:46:31 -0500

    overloaded opperators now return currency instead of changing current currency object added to_s function

--- currency.rb
+++ currency.rb
@@ -1,22 +1,37 @@
+require 'pry'
+
 class Currency
   attr_accessor:amount
   attr_accessor:type
 
-  def initialize(amount = 0.0, type = :"")
+  @@currency_types = {"$" => :USD, "¥" => :JPY, "£" => :GBP, "€" => :EUR}
+
+  def initialize(amount = 0.0, type = :USD)
     @amount = amount.to_f
     @type = type.to_sym
   end
 
+  class << self
+  private
+    def check_currency_symbol(value)
+      @@currency_types[value]
+    end
+  end
+
   def +(value)
-    @anount += value.amount
+    Currency.new(@amount += value.amount, @type)
   end
 
   def -(value)
-    @amount -= value
+    Currency.new(@amount -= value.amount, @type)
   end
 
   def ==(value)
     @amount == value.amount && @type == value.type
+  end
+
+  def !=(value)
+    @amount != value.amount || @type != value.type
   end
 
   def compare_currency_type(value)
@@ -24,10 +39,14 @@ class Currency
   end
 
   def *(value)
-    @amount *= value
+    Currency.new(@amount * value, @type)
   end
 
   def /(value)
-    @amount /= value
+    Currency.new(@amount / value, @type)
+  end
+
+  def to_s
+    @amount.to_s + " " + @type.to_s
   end
 end
